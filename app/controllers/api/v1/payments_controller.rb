d2c98769014b43917a19db4152af09a1368740e2
GusVilla303 <gusvilla303@gmail.com>
2015-11-05 22:22:46 -0700

    Create API controller for Payments

---
+++ app/controllers/api/v1/payments_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::PaymentsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    render json: 'not_found', status: :not_found
+  end
+
+  def index
+    render json: Payment.all
+  end
+
+  def show
+    render json: Payment.find(params[:id])
+  end
+
+  def create
+
+  end
+end
